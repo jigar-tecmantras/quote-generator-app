de3481111a754750baf61b50b04b213461d5ddfc
jigar-tecmantras <73215263+jigar-tecmantras@users.noreply.github.com>
2026-07-13 12:53:44 +0530

    Initialize quote generator app

---
+++ frontend/src/data/quotes.js
@@ -0,0 +1,79 @@
+const quotes = [
+  {
+    id: 'q1',
+    text: 'You miss 100% of the shots you don\'t take.',
+    author: 'Wayne Gretzky'
+  },
+  {
+    id: 'q2',
+    text: 'The best way to predict the future is to invent it.',
+    author: 'Alan Kay'
+  },
+  {
+    id: 'q3',
+    text: 'Simplicity is the ultimate sophistication.',
+    author: 'Leonardo da Vinci'
+  },
+  {
+    id: 'q4',
+    text: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+    author: 'Winston Churchill'
+  },
+  {
+    id: 'q5',
+    text: 'What we think, we become.',
+    author: 'Buddha'
+  },
+  {
+    id: 'q6',
+    text: 'Dream big and dare to fail.',
+    author: 'Norman Vaughan'
+  },
+  {
+    id: 'q7',
+    text: 'Do one thing every day that scares you.',
+    author: 'Eleanor Roosevelt'
+  },
+  {
+    id: 'q8',
+    text: 'The journey of a thousand miles begins with one step.',
+    author: 'Lao Tzu'
+  },
+  {
+    id: 'q9',
+    text: 'Stay hungry, stay foolish.',
+    author: 'Steve Jobs'
+  },
+  {
+    id: 'q10',
+    text: 'Creativity is intelligence having fun.',
+    author: 'Albert Einstein'
+  },
+  {
+    id: 'q11',
+    text: 'The only limit to our realization of tomorrow is our doubts of today.',
+    author: 'Franklin D. Roosevelt'
+  },
+  {
+    id: 'q12',
+    text: 'Happiness often sneaks in through a door you didn\'t know you left open.',
+    author: 'John Barrymore'
+  },
+  {
+    id: 'q13',
+    text: 'You are never too old to set another goal or to dream a new dream.',
+    author: 'C.S. Lewis'
+  },
+  {
+    id: 'q14',
+    text: 'In the middle of every difficulty lies opportunity.',
+    author: 'Albert Einstein'
+  },
+  {
+    id: 'q15',
+    text: 'Everything you can imagine is real.',
+    author: 'Pablo Picasso'
+  }
+];
+
+export default quotes;
